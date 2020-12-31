8b510dd8aff08d7c88f8ba791244cbe73363b675
Khinshan Khan <khanshan501@gmail.com>
2020-12-31 00:03:55 -0500

    processing SERVERS for ease of lookup

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,23 @@ import configJson from "../config/config.json";
 const { BOT, SERVERS } = configJson;
 const { DISCORD_TOKEN, REGULAR_PREFIX, EPHEMERAL_PREFIX } = BOT;
 
-// TODO: process SERVERS before export
+/* restructure server data to be in the form of:
+   {
+   [shortcut]: serverLink,
+   }
+*/
+const SERVERS_LOOKUP = Object.entries(SERVERS).reduce(
+    (stored, [serverLink, shortcuts]) => ({
+        ...stored,
+        ...shortcuts.reduce(
+            (merged, shortcut) => ({
+                ...merged,
+                [shortcut]: serverLink
+            }),
+            {}
+        )
+    }),
+    {}
+);
 
-export { DISCORD_TOKEN, REGULAR_PREFIX, EPHEMERAL_PREFIX, SERVERS };
+export { DISCORD_TOKEN, REGULAR_PREFIX, EPHEMERAL_PREFIX, SERVERS_LOOKUP };
--- src/utils/lookup.ts
+++ src/utils/lookup.ts
@@ -1,7 +1,7 @@
-import { SERVERS } from "./env";
+import { SERVERS_LOOKUP } from "./env";
 
 export const serverLookup = (potentialServer: string) =>
-    SERVERS[potentialServer as keyof typeof SERVERS];
+    SERVERS_LOOKUP[potentialServer as keyof typeof SERVERS_LOOKUP];
 
 // TODO: add in logic and separate into general vs restricted
 export const commandLookup = (potentialCommand: string) =>
